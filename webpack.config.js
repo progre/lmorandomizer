a0920410aa33131cc23c733559ef9c590749f3c0
progre <djyayutto@gmail.com>
2018-06-05 18:39:49 +0900

    feat: update to webpack4

--- webpack.config.js
+++ webpack.config.js
@@ -3,60 +3,62 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const uglifySaveLicense = require('uglify-save-license');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-const isProduction = process.env.NODE_ENV === 'production';
+module.exports = (_, argv) => {
+  const isProduction = argv.mode === 'production';
 
-const common = {
-  devtool: isProduction ? false : 'inline-source-map',
-  node: { __dirname: true, __filename: true },
-  resolve: { extensions: ['.ts', '.tsx', '.js'] },
-  watchOptions: { ignored: /node_modules|dist/ },
-};
+  const common = {
+    devtool: isProduction ? false : 'inline-source-map',
+    node: { __dirname: true, __filename: true },
+    resolve: { extensions: ['.ts', '.tsx', '.js'] },
+    watchOptions: { ignored: /node_modules|dist/ },
+  };
 
-const tsLoader = {
-  rules: [{
-    test: /\.tsx?$/,
-    use: [
-      {
-        loader: 'ts-loader',
-        options: { compilerOptions: { sourceMap: !isProduction } }
-      }
-    ]
-  }]
-};
-
-const clientSide = {
-  entry: {
-    index: './src/public/js/index.ts'
-  },
-  module: tsLoader,
-  output: { filename: 'dist/public/js/[name].js' },
-  plugins: [
-    new CopyWebpackPlugin(
-      [{ from: 'src/public/', to: 'dist/public/' }],
-      { ignore: ['test/', '*.ts', '*.tsx'] },
-    ),
-    ...(
-      !isProduction ? [] : [
-        new UglifyJsPlugin({
-          uglifyOptions: { output: { comments: uglifySaveLicense } },
-        }),
+  const tsLoader = {
+    rules: [{
+      test: /\.tsx?$/,
+      use: [
+        {
+          loader: 'ts-loader',
+          options: { compilerOptions: { sourceMap: !isProduction } }
+        }
       ]
-    )
-  ],
-  target: 'web',
-};
+    }]
+  };
 
-const serverSide = {
-  entry: {
-    index: './src/index.ts'
-  },
-  externals: /^(?!\.)/,
-  module: tsLoader,
-  output: { filename: 'dist/[name].js', libraryTarget: 'commonjs2' },
-  target: 'node',
-};
+  const clientSide = {
+    entry: {
+      index: './src/public/js/index.ts'
+    },
+    module: tsLoader,
+    output: { filename: 'public/js/[name].js' },
+    plugins: [
+      new CopyWebpackPlugin(
+        [{ from: 'src/public/', to: 'public/' }],
+        { ignore: ['test/', '*.ts', '*.tsx'] },
+      ),
+      ...(
+        !isProduction ? [] : [
+          new UglifyJsPlugin({
+            uglifyOptions: { output: { comments: uglifySaveLicense } },
+          }),
+        ]
+      )
+    ],
+    target: 'web',
+  };
 
-module.exports = [
-  { ...common, ...clientSide },
-  { ...common, ...serverSide },
-];
+  const serverSide = {
+    entry: {
+      index: './src/index.ts'
+    },
+    externals: /^(?!\.)/,
+    module: tsLoader,
+    output: { filename: '[name].js', libraryTarget: 'commonjs2' },
+    target: 'node',
+  };
+
+  return [
+    { ...common, ...clientSide },
+    { ...common, ...serverSide },
+  ];
+};
