2a28f7aa7ddcb41c60589e57ad9feb4f2279e349
progre <djyayutto@gmail.com>
2018-07-20 23:00:07 +0900

    refactor: extract to module

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,18 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const uglifySaveLicense = require('uglify-save-license');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const webpackConfig = require('@progre/webpack-config');
 
 module.exports = (_, argv) => {
   const isProduction = argv.mode === 'production';
-
-  const common = {
-    devtool: isProduction ? false : 'inline-source-map',
-    node: { __dirname: true, __filename: true },
-    resolve: { extensions: ['.ts', '.tsx', '.js'] },
-    watchOptions: { ignored: /node_modules|dist/ },
-  };
-
-  const tsLoader = {
-    rules: [{
-      test: /\.tsx?$/,
-      use: [
-        {
-          loader: 'ts-loader',
-          options: { compilerOptions: { sourceMap: !isProduction } }
-        }
-      ]
-    }]
-  };
-
-  const clientSide = {
-    entry: {
-      index: './src/public/js/index.ts'
-    },
-    module: tsLoader,
-    output: { filename: 'public/js/[name].js' },
-    plugins: [
-      new CopyWebpackPlugin(
-        [{ from: 'src/public/', to: 'public/' }],
-        { ignore: ['test/', '*.ts', '*.tsx'] },
-      ),
-      ...(
-        !isProduction ? [] : [
-          new UglifyJsPlugin({
-            uglifyOptions: { output: { comments: uglifySaveLicense } },
-          }),
-        ]
-      )
-    ],
-    target: 'web',
-  };
-
-  const serverSide = {
-    entry: {
-      index: './src/index.ts'
-    },
-    externals: /^(?!\.)/,
-    module: tsLoader,
-    output: { filename: '[name].js', libraryTarget: 'commonjs2' },
-    target: 'node',
-  };
-
   return [
-    { ...common, ...clientSide },
-    { ...common, ...serverSide },
+    webpackConfig.client(
+      isProduction,
+      'public/js/',
+      ['index.ts'],
+      true,
+    ),
+    webpackConfig.server(
+      isProduction,
+      '.',
+      ['index.ts'],
+    ),
   ];
 };
