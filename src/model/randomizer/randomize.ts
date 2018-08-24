bd9d0da0a0163a568e0db6c84c471863fe094d19
progre <djyayutto@gmail.com>
2018-08-25 08:18:14 +0900

    fix: fix travis

--- src/model/randomizer/randomize.ts
+++ src/model/randomizer/randomize.ts
@@ -55,37 +55,37 @@ function shuffle<T>(list: ReadonlyArray<T>) {
   return array;
 }
 
-function addStartingItems(txt: string, items: number[], subWeapons: number[]) {
-  const unusedOneTimeFlagNo = 7400;
-  const unusedSaveFlagNo = 6000;
-  let targetSeek = 0;
-  return txt.split('\n').map((x) => {
-    if (targetSeek === 0 && x === '<FIELD 1,1,1,1,0>') {
-      targetSeek = 1;
-      return x;
-    }
-    if (targetSeek === 1 && x === '<MAP 3,1,2>') {
-      targetSeek = 2;
-      return x;
-    }
-    if (targetSeek === 2 && x === '</MAP>') {
-      targetSeek = -1;
-      // tslint:disable:no-increment-decrement
-      return (
-        // tslint:disable-next-line:prefer-template
-        `<OBJECT 7,38912,14336,7,999,-1,-1></OBJECT>`
-        + `<OBJECT 22,26624,10240,2,2,${unusedOneTimeFlagNo},-1></OBJECT>`
-        + subWeapons.map(y => (
-          `<OBJECT 13,26624,10240,${y},0,${unusedSaveFlagNo},-1></OBJECT>`
-          + `<OBJECT 13,26624,10240,${y},255,${unusedSaveFlagNo},-1></OBJECT>`
-        )).join('')
-        + items.map(y => (
-          `<OBJECT 1,26624,14336,${unusedOneTimeFlagNo},${y},${unusedSaveFlagNo},-1></OBJECT>`
-        )).join('')
-        + x
-      );
-      // tslint:enable:no-increment-decrement
-    }
-    return x;
-  }).join('\n');
-}
+// function addStartingItems(txt: string, items: number[], subWeapons: number[]) {
+//   const unusedOneTimeFlagNo = 7400;
+//   const unusedSaveFlagNo = 6000;
+//   let targetSeek = 0;
+//   return txt.split('\n').map((x) => {
+//     if (targetSeek === 0 && x === '<FIELD 1,1,1,1,0>') {
+//       targetSeek = 1;
+//       return x;
+//     }
+//     if (targetSeek === 1 && x === '<MAP 3,1,2>') {
+//       targetSeek = 2;
+//       return x;
+//     }
+//     if (targetSeek === 2 && x === '</MAP>') {
+//       targetSeek = -1;
+//       // tslint:disable:no-increment-decrement
+//       return (
+//         // tslint:disable-next-line:prefer-template
+//         `<OBJECT 7,38912,14336,7,999,-1,-1></OBJECT>`
+//         + `<OBJECT 22,26624,10240,2,2,${unusedOneTimeFlagNo},-1></OBJECT>`
+//         + subWeapons.map(y => (
+//           `<OBJECT 13,26624,10240,${y},0,${unusedSaveFlagNo},-1></OBJECT>`
+//           + `<OBJECT 13,26624,10240,${y},255,${unusedSaveFlagNo},-1></OBJECT>`
+//         )).join('')
+//         + items.map(y => (
+//           `<OBJECT 1,26624,14336,${unusedOneTimeFlagNo},${y},${unusedSaveFlagNo},-1></OBJECT>`
+//         )).join('')
+//         + x
+//       );
+//       // tslint:enable:no-increment-decrement
+//     }
+//     return x;
+//   }).join('\n');
+// }
