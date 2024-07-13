c7be42d2b0c204e6e455c66ac2212d29aaa43ef7
progre <progre+pgp@prgrssv.net>
2024-07-13 11:51:25 +0900

    feat: add simple codec

---
+++ src-tauri/src/codec.rs
@@ -0,0 +1,30 @@
+use script::file::dat::{cipher_to_text, text_to_cipher};
+
+#[allow(unused)]
+mod dataset;
+#[allow(unused)]
+mod script;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() <= 3 {
+        eprintln!("Usage: lmocodec.exe [decode|encode] [input file] [output file]",);
+        std::process::exit(1);
+    }
+    let mode = &args[1];
+    if mode != "decode" && mode != "encode" {
+        eprintln!("Invalid mode: {}", mode);
+        std::process::exit(1);
+    }
+    let input_file_path = &args[2];
+    let output_file_path = &args[3];
+    if mode == "decode" {
+        let input_file = std::fs::read(input_file_path).unwrap();
+        let output = cipher_to_text(&input_file);
+        std::fs::write(output_file_path, output).unwrap();
+    } else {
+        let input_file = std::fs::read_to_string(input_file_path).unwrap();
+        let output = text_to_cipher(&input_file);
+        std::fs::write(output_file_path, output).unwrap();
+    }
+}
